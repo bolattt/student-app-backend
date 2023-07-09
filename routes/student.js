f2a53e439abbd0ede285b74386286791b105040e
Bo Latt <bolattny@gmail.com>
2023-07-09 14:12:37 -0400

    add status to response

--- routes/student.js
+++ routes/student.js
@@ -3,12 +3,22 @@ const students = express.Router();
 const studentData = require("../data/students.json");
 
 students.get("/", (req, res) => {
-  res.json(studentData);
+  try {
+    const { students } = studentData;
+    res.status(200).json({ data: students });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 students.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json(studentData.students[id]);
+  try {
+    const { id } = req.params;
+    const student = studentData.students.find((student) => student.id === id);
+    res.status(200).json({ data: student });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = students;
